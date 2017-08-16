e8ad899b4e1102ec28c7b0bd4ee78e341d65a3c9
Flavio Henrique <flavio.henso@gmail.com>
2017-08-16 15:54:00 -0300

    Carga inicial do sistema

---
+++ src/main/java/com/projetoSpringMVC/ProjetoSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package com.projetoSpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringMvcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projetoSpringMVC/ProjetoSpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projetoSpringMVC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoSpringMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
